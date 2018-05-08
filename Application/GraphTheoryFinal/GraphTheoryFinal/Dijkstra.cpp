34dc066bf5f43756229ae08577ea1750aa896381
Alex <ablair18@my.whitworth.edu>
2018-05-07 22:54:38 -0700

    Almost Completed Dijkstra's

--- Application/GraphTheoryFinal/GraphTheoryFinal/Dijkstra.cpp
+++ Application/GraphTheoryFinal/GraphTheoryFinal/Dijkstra.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <cstdio>
 #include <string>
 #include <queue>
+#include <limits.h>
+#include "Dijkstra.h"
+
 
 #define V 9
 
@@ -11,11 +15,50 @@ int minimum_path(int distance[], bool set[])
 
 	for(int v = 0; v<V; v++)
 	{
-		
+		if (set[v] == false && distance[v] <= min)
+		{
+			min = distance[v], min_index = v;
+		}
+	}
+	return min_index;
+}
+
+int Print_Solution(int distance[], int n)
+{
+	std::cout << "Vertex distance from source is" << std::endl;
+	for (int i = 0; i < V; i++)
+	{
+		sprintf("%d tt %d \n", i, distance[i]);
 	}
 }
 
 void dijkstra(int graph[V][V], int src)
 {
-	
+	int distance[V];
+
+	bool set[V];
+
+	for (int i = 0; i < V; i++)
+	{
+		distance[i] = INT_MAX, set[i] = false;
+
+		distance[src] = 0;
+
+	}
+
+	for (int count = 0; count < V - 1; count++)
+	{
+		int u = minimum_path(distance, set);
+
+		set[u] = true;
+
+		for (int b = 0; b < V; b++)
+		{
+			if (!set[b] && graph[u][b] && distance[u] != INT_MAX && distance[u] + graph[u][b] < distance[b])
+			{
+				distance[b] = distance[u] + graph[u][b];
+			}
+		}
+	}
+	Print_Solution(distance, V);
 }
---
+++ Application/GraphTheoryFinal/GraphTheoryFinal/Dijkstra.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+int minimum_path(int distance[], bool set[]);
+
+int Print_Solution(int distance[], int n);
+
+void dijkstra(int graph[V][V], int src);
+
