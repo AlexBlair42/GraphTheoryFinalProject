07d73b59db24d1a8ebf7078c0b64d300e178e951
Alex Blair <ablair18@my.whitworth.edu>
2018-05-07 20:37:38 -0700

    Set Up Project

---
+++ Application/GraphTheoryFinal/GraphTheoryFinal/Dijkstra.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+#define V 9
+
+int minimum_path(int distance[], bool set[])
+{
+	// Initialize minimums 
+	int min = INT_MAX, min_index;
+
+	for(int v = 0; v<V; v++)
+	{
+		
+	}
+}
+
+void dijkstra(int graph[V][V], int src)
+{
+	
+}
---
+++ Application/GraphTheoryFinal/GraphTheoryFinal/main.cpp
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////////
+// Alex Blair
+// MA 430
+// Final Project
+// Implementation of Dijkstra's Algorithm
+// As Well as Prim's/Kruskal's Algorithms
+// This file will read in a matrix of weights
+// Then run the data through each algorithm
+// And print time results as well as shortest path weight
+////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+
+
+	std::cin.get();
+}
